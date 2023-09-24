52942f4b1df73499c0036cd7622fa5a38d5a6817
Eduardo <eduardocorreaaranda10@gmail.com>
2023-09-24 21:23:06 +0200

    Ejercicio terminado

---
+++ src/main/java/termostato/Termostato.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package termostato;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eduar
+ */
+public class Termostato {
+
+    public static void main(String[] args) {
+        
+        //Creo el Scanner
+        Scanner teclado = new Scanner(System.in);
+        //Pido los datos por Scanner al usuario
+        System.out.print("Que grados Centigrados quieres convertir en "
+                + "Fahrenheit: ");
+        double cen = teclado.nextDouble();
+        //POngo como constantes los nuúmeros que no cambian en la secuencia
+        final int NUM1 = 9, NUM2 = 5, NUM3 = 32;
+        double resultado = (cen * NUM1 / NUM2) + NUM3;
+        //Imprimo el resultado por pantalla
+        System.out.println("El resultado de convertir " + cen + " centígrados "
+                + "en Fahrenheit es: " + resultado);
+    }
+}
